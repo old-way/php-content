eae456e954b9d460e80b759a0328f9e4387e7952
twilroad <269044570@qq.com>
2016-11-17 17:55:55 +0800

    一些修改

--- src/Abstracts/ArticleTemplateRegistrar.php
+++ src/Abstracts/ArticleTemplateRegistrar.php
@@ -1,41 +1,50 @@
 <?php
 /**
  * This file is part of Notadd.
+ *
  * @author TwilRoad <269044570@qq.com>
  * @copyright (c) 2016, iBenchu.org
  * @datetime 2016-10-10 16:53
  */
 namespace Notadd\Content\Abstracts;
+
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
 use Notadd\Content\Events\RegisterArticleTemplate as RegisterTemplateEvent;
 use Notadd\Content\Managers\ArticleManager;
 use Notadd\Foundation\Abstracts\EventSubscriber;
+
 /**
  * Class AbstractArticleTemplateRegistrar
- * @package Notadd\Content\Abstracts
  */
-abstract class ArticleTemplateRegistrar extends EventSubscriber {
+abstract class ArticleTemplateRegistrar extends EventSubscriber
+{
     /**
      * @var \Notadd\Content\Managers\ArticleManager
      */
     protected $manager;
+
     /**
      * AbstractArticleTemplateRegistrar constructor.
-     * @param \Illuminate\Container\Container $container
-     * @param \Illuminate\Events\Dispatcher $events
+     *
+     * @param \Illuminate\Container\Container         $container
+     * @param \Illuminate\Events\Dispatcher           $events
      * @param \Notadd\Content\Managers\ArticleManager $manager
      */
-    public function __construct(Container $container, Dispatcher $events, ArticleManager $manager) {
+    public function __construct(Container $container, Dispatcher $events, ArticleManager $manager)
+    {
         parent::__construct($container, $events);
         $this->manager = $manager;
     }
+
     /**
      * @return string
      */
-    protected function getEvent() {
+    protected function getEvent()
+    {
         return RegisterTemplateEvent::class;
     }
+
     /**
      * @return void
      */
