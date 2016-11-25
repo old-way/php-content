b75f258f734eb5ad0b626e723c20090e450cf550
twilroad <269044570@qq.com>
2016-11-25 15:25:17 +0800

    添加ArticleType、CategoryType、PageType相关Create、Delete、Edit、Find的Handler

---
+++ src/Handlers/ArticleTypeCreateHandler.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-11-25 15:18
+ */
+
+namespace Notadd\Content\Handlers;
+
+/**
+ * Class ArticleTypeCreateHandler.
+ */
+class ArticleTypeCreateHandler
+{
+}
---
+++ src/Handlers/CategoryTypeCreateHandler.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2016, iBenchu.org
+ * @datetime 2016-11-25 15:19
+ */
+
+namespace Notadd\Content\Handlers;
+
+/**
+ * Class CategoryTypeCreateHandler.
+ */
+class CategoryTypeCreateHandler
+{
+}
