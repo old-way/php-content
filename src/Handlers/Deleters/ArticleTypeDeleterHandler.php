a587065fee8d23dd94ece07b07a78a0f0e6c0779
twilroad <269044570@qq.com>
2016-11-30 18:30:45 +0800

    实现ArticleTypeDeleter的Api

--- src/Handlers/Deleters/ArticleTypeDeleterHandler.php
+++ src/Handlers/Deleters/ArticleTypeDeleterHandler.php
@@ -8,6 +8,10 @@
  */
 namespace Notadd\Content\Handlers\Deleters;
 
+use Illuminate\Container\Container;
+use Illuminate\Http\Request;
+use Illuminate\Translation\Translator;
+use Notadd\Content\Models\ArticleType;
 use Notadd\Foundation\Passport\Abstracts\SetHandler;
 
 /**
@@ -15,6 +19,29 @@ use Notadd\Foundation\Passport\Abstracts\SetHandler;
  */
 class ArticleTypeDeleterHandler extends SetHandler
 {
+    /**
+     * @var \Notadd\Content\Models\ArticleType
+     */
+    protected $articleType;
+
+    /**
+     * ArticleTypeDeleterHandler constructor.
+     *
+     * @param \Notadd\Content\Models\ArticleType $articleType
+     * @param \Illuminate\Container\Container    $container
+     * @param \Illuminate\Http\Request           $request
+     * @param \Illuminate\Translation\Translator $translator
+     */
+    public function __construct(
+        ArticleType $articleType,
+        Container $container,
+        Request $request,
+        Translator $translator
+    ) {
+        parent::__construct($container, $request, $translator);
+        $this->articleType = $articleType;
+    }
+
     /**
      * @return int
      */
@@ -38,6 +65,12 @@ class ArticleTypeDeleterHandler extends SetHandler
      */
     public function execute()
     {
+        $articleType = $this->articleType->newQuery()->find($this->request->input('id'));
+        if ($articleType === null) {
+            return false;
+        }
+        $articleType->delete();
+
         return true;
     }
 
