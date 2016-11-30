e10fffe87f3ff2b1feaaa6132184b766d9afbb3a
twilroad <269044570@qq.com>
2016-11-30 19:01:25 +0800

    实现CategoryTemplateEditor的Api

--- src/Handlers/Editors/CategoryTemplateEditorHandler.php
+++ src/Handlers/Editors/CategoryTemplateEditorHandler.php
@@ -8,6 +8,10 @@
  */
 namespace Notadd\Content\Handlers\Editors;
 
+use Illuminate\Container\Container;
+use Illuminate\Http\Request;
+use Illuminate\Translation\Translator;
+use Notadd\Content\Models\CategoryTemplate;
 use Notadd\Foundation\Passport\Abstracts\SetHandler;
 
 /**
@@ -15,6 +19,29 @@ use Notadd\Foundation\Passport\Abstracts\SetHandler;
  */
 class CategoryTemplateEditorHandler extends SetHandler
 {
+    /**
+     * @var \Notadd\Content\Models\CategoryTemplate
+     */
+    protected $categoryTemplate;
+
+    /**
+     * CategoryTemplateEditorHandler constructor.
+     *
+     * @param \Notadd\Content\Models\CategoryTemplate $categoryTemplate
+     * @param \Illuminate\Container\Container         $container
+     * @param \Illuminate\Http\Request                $request
+     * @param \Illuminate\Translation\Translator      $translator
+     */
+    public function __construct(
+        CategoryTemplate $categoryTemplate,
+        Container $container,
+        Request $request,
+        Translator $translator
+    ) {
+        parent::__construct($container, $request, $translator);
+        $this->categoryTemplate = $categoryTemplate;
+    }
+
     /**
      * @return int
      */
@@ -38,6 +65,12 @@ class CategoryTemplateEditorHandler extends SetHandler
      */
     public function execute()
     {
+        $categoryTemplate = $this->categoryTemplate->newQuery()->find($this->request->input('id'));
+        if ($categoryTemplate === null) {
+            return false;
+        }
+        $categoryTemplate->update($this->request->all());
+
         return true;
     }
 
