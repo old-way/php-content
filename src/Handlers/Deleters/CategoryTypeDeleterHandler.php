cd217218c49108c7b6d741b0b63b7e7a06b19049
twilroad <269044570@qq.com>
2016-11-30 18:36:17 +0800

    实现CategoryTypeDeleter的Api

--- src/Handlers/Deleters/CategoryTypeDeleterHandler.php
+++ src/Handlers/Deleters/CategoryTypeDeleterHandler.php
@@ -8,6 +8,10 @@
  */
 namespace Notadd\Content\Handlers\Deleters;
 
+use Illuminate\Container\Container;
+use Illuminate\Http\Request;
+use Illuminate\Translation\Translator;
+use Notadd\Content\Models\CategoryType;
 use Notadd\Foundation\Passport\Abstracts\SetHandler;
 
 /**
@@ -15,6 +19,29 @@ use Notadd\Foundation\Passport\Abstracts\SetHandler;
  */
 class CategoryTypeDeleterHandler extends SetHandler
 {
+    /**
+     * @var \Notadd\Content\Models\CategoryType
+     */
+    protected $categoryType;
+
+    /**
+     * CategoryTypeDeleterHandler constructor.
+     *
+     * @param \Notadd\Content\Models\CategoryType $categoryType
+     * @param \Illuminate\Container\Container     $container
+     * @param \Illuminate\Http\Request            $request
+     * @param \Illuminate\Translation\Translator  $translator
+     */
+    public function __construct(
+        CategoryType $categoryType,
+        Container $container,
+        Request $request,
+        Translator $translator
+    ) {
+        parent::__construct($container, $request, $translator);
+        $this->categoryType = $categoryType;
+    }
+
     /**
      * @return int
      */
@@ -38,6 +65,12 @@ class CategoryTypeDeleterHandler extends SetHandler
      */
     public function execute()
     {
+        $categoryType = $this->categoryType->newQuery()->find($this->request->input('id'));
+        if ($categoryType === null) {
+            return false;
+        }
+        $categoryType->delete();
+
         return true;
     }
 
