99cbb0da0424af2ce5d7a196fb5d0a14b7fe0b37
twilroad <269044570@qq.com>
2016-11-30 19:21:04 +0800

    实现CategoryTemplateFinder的Api

--- src/Handlers/Finders/CategoryTemplateFinderHandler.php
+++ src/Handlers/Finders/CategoryTemplateFinderHandler.php
@@ -8,6 +8,10 @@
  */
 namespace Notadd\Content\Handlers\Finders;
 
+use Illuminate\Container\Container;
+use Illuminate\Http\Request;
+use Illuminate\Translation\Translator;
+use Notadd\Content\Models\CategoryTemplate;
 use Notadd\Foundation\Passport\Abstracts\DataHandler;
 
 /**
@@ -15,6 +19,29 @@ use Notadd\Foundation\Passport\Abstracts\DataHandler;
  */
 class CategoryTemplateFinderHandler extends DataHandler
 {
+    /**
+     * @var \Notadd\Content\Models\CategoryTemplate
+     */
+    protected $categoryTemplate;
+
+    /**
+     * CategoryTemplateFinderHandler constructor.
+     *
+     * @param \Notadd\Content\Models\CategoryTemplate $categoryTemplate
+     * @param \Illuminate\Container\Container         $container
+     * @param \Illuminate\Http\Request                $request
+     * @param \Illuminate\Translation\Translator      $translator
+     */
+    public function __construct(
+        CategoryTemplate $categoryTemplate,
+        Container $container,
+        Request $request,
+        Translator $translator
+    ) {
+        parent::__construct($container, $request, $translator);
+        $this->categoryTemplate = $categoryTemplate;
+    }
+
     /**
      * @return int
      */
@@ -28,7 +55,9 @@ class CategoryTemplateFinderHandler extends DataHandler
      */
     public function data()
     {
-        return [];
+        $categoryTemplate = $this->categoryTemplate->newQuery()->find($this->request->input('id'));
+
+        return $categoryTemplate->getAttributes();
     }
 
     /**
