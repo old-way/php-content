3c7cb89be03798085cf8b4411be740885e80f55d
twilroad <269044570@qq.com>
2016-11-30 19:24:52 +0800

    实现PageTemplateFinder的Api

--- src/Handlers/Finders/PageTemplateFinderHandler.php
+++ src/Handlers/Finders/PageTemplateFinderHandler.php
@@ -8,6 +8,10 @@
  */
 namespace Notadd\Content\Handlers\Finders;
 
+use Illuminate\Container\Container;
+use Illuminate\Http\Request;
+use Illuminate\Translation\Translator;
+use Notadd\Content\Models\PageTemplate;
 use Notadd\Foundation\Passport\Abstracts\DataHandler;
 
 /**
@@ -15,6 +19,29 @@ use Notadd\Foundation\Passport\Abstracts\DataHandler;
  */
 class PageTemplateFinderHandler extends DataHandler
 {
+    /**
+     * @var \Notadd\Content\Models\PageTemplate
+     */
+    protected $pageTemplate;
+
+    /**
+     * PageTemplateFinderHandler constructor.
+     *
+     * @param \Illuminate\Container\Container     $container
+     * @param \Notadd\Content\Models\PageTemplate $pageTemplate
+     * @param \Illuminate\Http\Request            $request
+     * @param \Illuminate\Translation\Translator  $translator
+     */
+    public function __construct(
+        Container $container,
+        PageTemplate $pageTemplate,
+        Request $request,
+        Translator $translator
+    ) {
+        parent::__construct($container, $request, $translator);
+        $this->pageTemplate = $pageTemplate;
+    }
+
     /**
      * @return int
      */
@@ -28,7 +55,9 @@ class PageTemplateFinderHandler extends DataHandler
      */
     public function data()
     {
-        return [];
+        $pageTemplate = $this->pageTemplate->newQuery()->find($this->request->input('id'));
+
+        return $pageTemplate->getAttributes();
     }
 
     /**
