64fcd7007d6f66aae87ec8a89eeff4398c1c4d68
twilroad <269044570@qq.com>
2016-12-08 20:06:09 +0800

    Add a article type show handler

--- src/Controllers/Api/ArticleTypeController.php
+++ src/Controllers/Api/ArticleTypeController.php
@@ -8,6 +8,7 @@
  */
 namespace Notadd\Content\Controllers\Api;
 
+use Notadd\Content\Handlers\Finders\ArticleTypeFinderHandler;
 use Notadd\Foundation\Routing\Abstracts\Controller;
 
 /**
@@ -15,4 +16,18 @@ use Notadd\Foundation\Routing\Abstracts\Controller;
  */
 class ArticleTypeController extends Controller
 {
+    /**
+     * Add a article type show handler.
+     *
+     * @param \Notadd\Content\Handlers\Finders\ArticleTypeFinderHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse * @throws \Exception
+     * @throws \Exception
+     */
+    public function show(ArticleTypeFinderHandler $handler)
+    {
+        $response = $handler->toResponse();
+
+        return $response->generateHttpResponse();
+    }
 }
