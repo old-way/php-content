79a82c7d18281f6c6edbde1d5260b85d8c43c082
twilroad <269044570@qq.com>
2016-12-08 20:08:50 +0800

    Add a category template show handler

--- src/Controllers/Api/CategoryTemplateController.php
+++ src/Controllers/Api/CategoryTemplateController.php
@@ -8,6 +8,7 @@
  */
 namespace Notadd\Content\Controllers\Api;
 
+use Notadd\Content\Handlers\Finders\CategoryTemplateFinderHandler;
 use Notadd\Foundation\Routing\Abstracts\Controller;
 
 /**
@@ -15,4 +16,18 @@ use Notadd\Foundation\Routing\Abstracts\Controller;
  */
 class CategoryTemplateController extends Controller
 {
+    /**
+     * Add a category template show handler.
+     *
+     * @param \Notadd\Content\Handlers\Finders\CategoryTemplateFinderHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse * @throws \Exception
+     * @throws \Exception
+     */
+    public function show(CategoryTemplateFinderHandler $handler)
+    {
+        $response = $handler->toResponse();
+
+        return $response->generateHttpResponse();
+    }
 }
