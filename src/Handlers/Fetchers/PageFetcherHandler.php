dc3b483b27eef4323a3498115d820232784786f9
twilroad <269044570@qq.com>
2017-01-15 20:37:56 +0800

    add fetchers related to page

---
+++ src/Handlers/Fetchers/PageFetcherHandler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-01-15 20:36
+ */
+namespace Notadd\Content\Handlers\Fetchers;
+
+/**
+ * Class PageFetcherHandler.
+ */
+class PageFetcherHandler
+{
+}
