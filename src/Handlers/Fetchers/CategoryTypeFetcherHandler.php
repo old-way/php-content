c567abf10ce8023f82632555bc799d936f44c397
twilroad <269044570@qq.com>
2017-01-15 20:35:58 +0800

    add fetchers related to category

---
+++ src/Handlers/Fetchers/CategoryTypeFetcherHandler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-01-15 20:35
+ */
+namespace Notadd\Content\Handlers\Fetchers;
+
+/**
+ * Class CategoryTypeFetcherHandler.
+ */
+class CategoryTypeFetcherHandler
+{
+}
