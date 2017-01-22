ef4f69c729e436b455bc0ab1fb3de46246c23a72
twilroad <269044570@qq.com>
2017-01-22 18:20:44 +0800

    add migration file for table pages

---
+++ databases/migrations/2017_01_22_181017_create_pages_table.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @datetime 2017-01-22 18:10:17
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Notadd\Foundation\Database\Migrations\Migration;
+
+/**
+ * Class CreatePagesTable.
+ */
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id');
+            $table->string('title');
+            $table->string('thumb_image')->nullable();
+            $table->string('alias')->nullable();
+            $table->string('keyword')->nullable();
+            $table->string('description')->nullable();
+            $table->string('template')->nullable();
+            $table->text('content')->nullable();
+            $table->boolean('enabled')->default(true);
+            $table->bigInteger('hits')->default(0);
+            $table->tinyInteger('order_id')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->drop('pages');
+    }
+}
