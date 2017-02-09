7230896c7be6652b17413def175332199a0e4553
twilroad <269044470@qq.com>
2017-02-09 17:11:57 +0800

    add table categories's migration file

---
+++ databases/migrations/2017_02_09_170514_create_categories_table.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @datetime 2017-02-09 17:05:14
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Notadd\Foundation\Database\Migrations\Migration;
+
+/**
+ * Class CreateCategoriesTable.
+ */
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id');
+            $table->string('title');
+            $table->string('alias')->nullable();
+            $table->string('description')->nullable();
+            $table->string('type')->default('normal');
+            $table->string('seo_title')->nullable();
+            $table->string('seo_keyword')->nullable();
+            $table->string('seo_description')->nullable();
+            $table->string('background_color')->nullable();
+            $table->string('background_image')->nullable();
+            $table->string('top_image')->nullable();
+            $table->tinyInteger('pagination')->default(30);
+            $table->boolean('enabled')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->drop('categories');
+    }
+}
