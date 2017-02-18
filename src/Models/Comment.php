1e53e88b7e33d82297147723fdc57462401fc5d5
twilroad <269044570@qq.com>
2017-02-18 16:31:15 +0800

    add migration file and model file for table comments

---
+++ src/Models/Comment.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-02-18 16:24
+ */
+namespace Notadd\Content\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Comment.
+ */
+class Comment extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'log_id',
+        'user_id',
+        'action',
+        'date',
+        'post_id',
+        'thread_id',
+        'thread_key',
+        'author_id',
+        'author_name',
+        'author_email',
+        'author_url',
+        'author_key',
+        'ip',
+        'created_at',
+        'message',
+        'parent_id',
+        'type',
+    ];
+}
