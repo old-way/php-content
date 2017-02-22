b554c120b5f4c965466fdac5f0e76246050df1f9
twilroad <269044570@qq.com>
2017-02-22 16:41:01 +0800

    correct name of stylesheet file

--- resources/mixes/administration/build/webpack.prod.conf.js
+++ resources/mixes/administration/build/webpack.prod.conf.js
@@ -31,7 +31,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       sourceMap: true
     }),
     new ExtractTextPlugin({
-      filename: utils.assetsPath('assets/content/administration/css/[name].css')
+      filename: utils.assetsPath('assets/content/administration/css/module.css')
     }),
     new OptimizeCSSPlugin()
   ]
