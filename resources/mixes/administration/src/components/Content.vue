e2c9ff142c0ba72c4d537c536920791c1ec5120e
twilroad <269044570@qq.com>
2017-02-22 15:38:52 +0800

    move assets files from administration to content

---
+++ resources/mixes/administration/src/components/Content.vue
@@ -0,0 +1,16 @@
+<script>
+  export default {
+    methods: {
+      submit: function (e) {
+        let _this = this
+        _this.$validator.validateAll()
+        if (_this.errors.any()) {
+          return false
+        }
+      }
+    }
+  }
+</script>
+<template>
+    <div class="box box-solid"></div>
+</template>
