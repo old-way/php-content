55ce46786426fcadb41d67ba5b4903b3461fafbb
twilroad <269044570@qq.com>
2017-02-28 18:48:35 +0800

    add progress status while create article

--- resources/mixes/administration/src/components/ArticleCreate.vue
+++ resources/mixes/administration/src/components/ArticleCreate.vue
@@ -77,6 +77,7 @@
         if (_this.errors.any()) {
           return false
         }
+        _this.$store.state.commit('progress', 'start')
         const _formData = new window.FormData()
         _formData.append('category_id', _this.category.id)
         _formData.append('content', _this.content)
@@ -103,7 +104,10 @@
               time: 3000,
               type: 'notice'
             })
+            _this.$store.state.commit('progress', 'done')
           }
+        }).catch(() => {
+          _this.$store.state.commit('progress', 'fail')
         })
       }
     },
