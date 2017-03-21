e0d7f71833089b298d68e9a894084af6bca86e66
twilroad <269044570@qq.com>
2017-03-21 10:59:20 +0800

    add routes definitions

--- resources/mixes/view/src/mixes/router.js
+++ resources/mixes/view/src/mixes/router.js
@@ -1,5 +1,17 @@
+import Article from '../pages/Article';
+import ArticleCategory from '../pages/ArticleCategory';
+import ArticleCreate from '../pages/ArticleCreate';
+import ArticleDraft from '../pages/ArticleDraft';
+import ArticleDraftEdit from '../pages/ArticleDraftEdit';
+import ArticleEdit from '../pages/ArticleEdit';
+import ArticleRecycle from '../pages/ArticleRecycle';
+import Component from '../pages/Component';
 import Dashboard from '../pages/Dashboard';
 import Layout from '../layouts/Layout';
+import Page from '../pages/Page';
+import PageCategory from '../pages/PageCategory';
+import PageCreate from '../pages/PageCreate';
+import PageEdit from '../pages/PageEdit';
 
 export default function (injection) {
     injection.useModuleRoute([
@@ -9,6 +21,66 @@ export default function (injection) {
                     beforeEnter: injection.middleware.requireAuth,
                     component: Dashboard,
                     path: '/',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: Article,
+                    path: 'article',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: ArticleCategory,
+                    path: 'article/category',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: ArticleCreate,
+                    path: 'article/create',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: ArticleEdit,
+                    path: 'article/:id/edit',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: ArticleDraft,
+                    path: 'article/draft',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: ArticleDraftEdit,
+                    path: 'article/:id/draft',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: ArticleRecycle,
+                    path: 'article/recycle',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: Component,
+                    path: 'component',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: Page,
+                    path: 'page',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: PageCategory,
+                    path: 'page/category',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: PageCreate,
+                    path: 'page/create',
+                },
+                {
+                    beforeEnter: injection.middleware.requireAuth,
+                    component: PageEdit,
+                    path: 'page/:id/edit',
                 },
             ],
             component: Layout,
---
+++ resources/mixes/view/src/pages/Component.vue
@@ -0,0 +1,12 @@
+<script>
+    import injection from '../helpers/injection';
+
+    export default {
+        beforeRouteEnter(to, from, next) {
+            next(() => {
+                injection.sidebar.active('content');
+            });
+        },
+    }
+</script>
+<template></template>
