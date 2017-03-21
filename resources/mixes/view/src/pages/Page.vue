1372fed8855563cac02733ff0d64a0677b1a52dc
twilroad <269044570@qq.com>
2017-03-21 10:48:29 +0800

    add sidebar active codes for vue files

--- resources/mixes/view/src/pages/Page.vue
+++ resources/mixes/view/src/pages/Page.vue
@@ -1,4 +1,12 @@
 <script>
-    export default {}
+    import injection from '../helpers/injection';
+
+    export default {
+        beforeRouteEnter(to, from, next) {
+            next(() => {
+                injection.sidebar.active('content');
+            });
+        },
+    }
 </script>
 <template></template>
