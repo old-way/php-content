576f6854af02f785af32e3d1fba52a7186cdd75d
twilroad <269044570@qq.com>
2017-04-11 18:47:25 +0800

    add form with translations

--- resources/translations/en-us/article.php
+++ resources/translations/en-us/article.php
@@ -6,8 +6,8 @@
  * @copyright (c) 2017, iBenchu.org
  * @datetime 2017-01-16 15:00
  */
-
 return [
+    'add'    => '添加文章',
     'create' => [
         'success' => 'Article has been created!',
         'fail'    => 'Article create fail!',
@@ -23,6 +23,42 @@ return [
     'find'   => [
         'success' => 'Article has been found!',
         'fail'    => 'Article find fail!',
+    ],
+    'form'   => [
+        'category' => [
+            'label' => '分类',
+        ],
+        'content'  => [
+            'error' => '请输入文章内容！',
+        ],
+        'date'     => [
+            'label'       => '发布时间',
+            'placeholder' => '请选择发布时间',
+        ],
+        'hidden'   => [
+            'close' => '取消',
+            'label' => '隐藏',
+            'open'  => '隐藏',
+        ],
+        'source'   => [
+            'author' => [
+                'label'       => '来源',
+                'placeholder' => '请输入来源',
+            ],
+            'link'   => [
+                'label'       => '来源链接',
+                'placeholder' => '请输入来源链接',
+            ],
+        ],
+        'sticky'   => [
+            'close' => '取消',
+            'label' => '置顶',
+            'open'  => '置顶',
+        ],
+        'title'    => [
+            'error'       => '请输入文章标题！',
+            'placeholder' => '请输入文章标题',
+        ],
     ],
     'update' => [
         'success' => 'Article has been updated!',
