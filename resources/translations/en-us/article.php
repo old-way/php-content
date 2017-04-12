9d16f143a0c9fa995502172a1d395ff547dd8ec5
twilroad <269044570@qq.com>
2017-04-12 12:03:20 +0800

    fix wrong location for translation codes

--- resources/translations/en-us/article.php
+++ resources/translations/en-us/article.php
@@ -7,7 +7,6 @@
  * @datetime 2017-01-16 15:00
  */
 return [
-    'add'    => '添加文章',
     'create' => [
         'success' => 'Article has been created!',
         'fail'    => 'Article create fail!',
@@ -23,47 +22,6 @@ return [
     'find'   => [
         'success' => 'Article has been found!',
         'fail'    => 'Article find fail!',
-    ],
-    'form'   => [
-        'category' => [
-            'label' => '分类',
-        ],
-        'content'  => [
-            'error' => '请输入文章内容！',
-        ],
-        'date'     => [
-            'label'       => '发布时间',
-            'placeholder' => '请选择发布时间',
-        ],
-        'hidden'   => [
-            'close' => '取消',
-            'label' => '隐藏',
-            'open'  => '隐藏',
-        ],
-        'source'   => [
-            'author' => [
-                'label'       => '来源',
-                'placeholder' => '请输入来源',
-            ],
-            'link'   => [
-                'label'       => '来源链接',
-                'placeholder' => '请输入来源链接',
-            ],
-        ],
-        'sticky'   => [
-            'close' => '取消',
-            'label' => '置顶',
-            'open'  => '置顶',
-        ],
-        'title'    => [
-            'error'       => '请输入文章标题！',
-            'placeholder' => '请输入文章标题',
-        ],
-    ],
-    'table'  => [
-        'date'   => '文章名称',
-        'handle' => '操作',
-        'title'  => '文章名称',
     ],
     'update' => [
         'success' => 'Article has been updated!',
--- resources/translations/zh-cn/article.php
+++ resources/translations/zh-cn/article.php
@@ -6,8 +6,8 @@
  * @copyright (c) 2017, iBenchu.org
  * @datetime 2017-01-16 15:00
  */
-
 return [
+    'add'    => '添加文章',
     'create' => [
         'success' => '文章已经被创建！',
         'fail'    => '文章创建失败！',
@@ -24,13 +24,50 @@ return [
         'success' => '文章查找成功！',
         'fail'    => '文章查找失败！',
     ],
-    'force' => [
+    'force'  => [
         'success' => '文章已经被强制删除！',
         'fail'    => '文章强制删除失败！',
     ],
-    'find'   => [
-        'success' => '文章查找成功！',
-        'fail'    => '文章查找失败！',
+    'form'   => [
+        'category' => [
+            'label' => '分类',
+        ],
+        'content'  => [
+            'error' => '请输入文章内容！',
+        ],
+        'date'     => [
+            'label'       => '发布时间',
+            'placeholder' => '请选择发布时间',
+        ],
+        'hidden'   => [
+            'close' => '取消',
+            'label' => '隐藏',
+            'open'  => '隐藏',
+        ],
+        'source'   => [
+            'author' => [
+                'label'       => '来源',
+                'placeholder' => '请输入来源',
+            ],
+            'link'   => [
+                'label'       => '来源链接',
+                'placeholder' => '请输入来源链接',
+            ],
+        ],
+        'sticky'   => [
+            'close' => '取消',
+            'label' => '置顶',
+            'open'  => '置顶',
+        ],
+        'title'    => [
+            'error'       => '请输入文章标题！',
+            'placeholder' => '请输入文章标题',
+        ],
+    ],
+    'table'  => [
+        'date'   => '文章名称',
+        'handle' => '操作',
+        'title'  => '文章名称',
     ],
     'update' => [
         'success' => '文章已经被更新！',
