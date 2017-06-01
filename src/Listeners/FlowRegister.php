ae74401972b31c86796fee539afac06f06788250
twilroad <269044570@qq.com>
2017-06-01 18:46:50 +0800

    add entities

--- src/Listeners/FlowRegister.php
+++ src/Listeners/FlowRegister.php
@@ -8,6 +8,11 @@
  */
 namespace Notadd\Content\Listeners;
 
+use Notadd\Content\Entities\Article;
+use Notadd\Content\Entities\ArticleCategory;
+use Notadd\Content\Entities\ArticleDraft;
+use Notadd\Content\Entities\Page;
+use Notadd\Content\Entities\PageCategory;
 use Notadd\Foundation\Flow\Abstracts\FlowRegister as AbstractFlowRegister;
 
 /**
@@ -26,10 +31,10 @@ class FlowRegister extends AbstractFlowRegister
      */
     public function handle()
     {
-        foreach ($this->definitions as $definition) {
-            if (method_exists($this, 'register' . ucfirst($definition) . 'Flow')) {
-                $this->{'register' . ucfirst($definition) . 'Flow'}();
-            }
-        }
+        $this->flow->register(Article::class);
+        $this->flow->register(ArticleCategory::class);
+        $this->flow->register(ArticleDraft::class);
+        $this->flow->register(Page::class);
+        $this->flow->register(PageCategory::class);
     }
 }
