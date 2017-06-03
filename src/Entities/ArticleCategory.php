e5afc278e890e83a9f10f3bebcb0274e94ef7eaa
twilroad <269044570@qq.com>
2017-06-03 14:26:16 +0800

    add details of places and transitions for article category flow

--- src/Entities/ArticleCategory.php
+++ src/Entities/ArticleCategory.php
@@ -9,20 +9,13 @@
 namespace Notadd\Content\Entities;
 
 use Notadd\Foundation\Flow\Abstracts\Entity;
+use Symfony\Component\Workflow\Transition;
 
 /**
  * Class ArticleCategory.
  */
 class ArticleCategory extends Entity
 {
-    /**
-     * @return array
-     */
-    public function events()
-    {
-        return [];
-    }
-
     /**
      * @return string
      */
@@ -36,7 +29,14 @@ class ArticleCategory extends Entity
      */
     public function places()
     {
-        return [];
+        return [
+            'create',
+            'created',
+            'edit',
+            'edited',
+            'remove',
+            'removed',
+        ];
     }
 
     /**
@@ -44,7 +44,11 @@ class ArticleCategory extends Entity
      */
     public function transitions()
     {
-        return [];
+        return [
+            new Transition('create', 'create', 'created'),
+            new Transition('edit', 'edit', 'edited'),
+            new Transition('remove', 'remove', 'removed'),
+        ];
     }
 
     /**
