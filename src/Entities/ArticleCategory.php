08f319d4a1e5f2e28053ac86c09fe02f20905a16
twilroad <269044570@qq.com>
2017-06-13 18:19:59 +0800

    add guard logic for article category flow

--- src/Entities/ArticleCategory.php
+++ src/Entities/ArticleCategory.php
@@ -85,7 +85,25 @@ class ArticleCategory extends Entity
      */
     public function guard(GuardEvent $event)
     {
-        // TODO: Implement guard() method.
+        switch ($event->getTransition()->getName()) {
+            case 'create':
+                $this->block($event, $this->permission(''));
+                break;
+            case 'need_to_edit':
+                $this->block($event, $this->permission(''));
+                break;
+            case 'edit':
+                $this->block($event, $this->permission(''));
+                break;
+            case 'need_to_remove':
+                $this->block($event, $this->permission(''));
+                break;
+            case 'remove':
+                $this->block($event, $this->permission(''));
+                break;
+            default:
+                $event->setBlocked(true);
+        }
     }
 
     /**
