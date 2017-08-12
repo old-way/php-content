739a2c08f2804b2b51e36de5717ed37e69d8dbf4
twilroad <269044570@qq.com>
2017-08-12 13:19:48 +0800

    add migration file and model for table content_article_informations

---
+++ databases/migrations/2017_08_12_131201_create_content_article_informations_table.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @datetime 2017-08-12 13:12:01
+ */
+use Illuminate\Database\Schema\Blueprint;
+use Notadd\Foundation\Database\Migrations\Migration;
+
+/**
+ * Class CreateContentArticleInformationsTable.
+ */
+class CreateContentArticleInformationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('content_article_informations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description')->nullable();
+            $table->tinyInteger('details')->default(0);
+            $table->tinyInteger('length')->default(0);
+            $table->string('name');
+            $table->tinyInteger('order')->default(0);
+            $table->string('opinions')->nullable();
+            $table->tinyInteger('privacy')->default(0);
+            $table->tinyInteger('register')->default(0);
+            $table->tinyInteger('required')->default(0);
+            $table->enum('type', [
+                'checkbox',
+                'date',
+                'daterange',
+                'dropdown',
+                'datetime',
+                'file',
+                'input',
+                'picture',
+                'radio',
+                'select',
+                'textarea',
+            ]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->drop('content_article_informations');
+    }
+}
---
+++ src/Models/ArticleInformation.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-08-12 13:18
+ */
+
+namespace Notadd\Content\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class ArticleInformation.
+ */
+class ArticleInformation extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * @var string
+     */
+    protected $table = 'content_article_informations';
+}
