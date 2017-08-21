10df38984cfb6d349bae6a27bcfca23ad4f3e599
twilroad <269044570@qq.com>
2017-08-21 22:34:48 +0800

    add definitions of publishes

--- src/ModuleServiceProvider.php
+++ src/ModuleServiceProvider.php
@@ -34,10 +34,6 @@ class ModuleServiceProvider extends Module
         $this->loadMigrationsFrom(realpath(__DIR__ . '/../databases/migrations'));
         $this->loadTranslationsFrom(realpath(__DIR__ . '/../resources/translations'), 'content');
         $this->loadViewsFrom(realpath(__DIR__ . '/../resources/views'), 'content');
-        $this->publishes([
-            realpath(__DIR__ . '/../resources/mixes/administration/dist/assets/content/administration') => public_path('assets/content/administration'),
-            realpath(__DIR__ . '/../resources/mixes/foreground/dist/assets/content/foreground') => public_path('assets/content/foreground'),
-        ], 'public');
     }
 
     /**
