d45a20e94bd2fb82bf9ac9a9fcaecfba68c9853a
twilroad <269044570@qq.com>
2017-08-22 16:03:21 +0800

    rebuild assets files

--- resources/mixes/administration/build/build.js
+++ resources/mixes/administration/build/build.js
@@ -2,13 +2,14 @@ require('./check-versions')();
 
 process.env.NODE_ENV = 'production';
 
-var rm = require('rimraf');
-var path = require('path');
-var chalk = require('chalk');
-var merge = require('webpack-merge');
-var webpack = require('webpack');
-var config = require('../config');
 var buildWebpackConfig = require('./webpack.prod.conf');
+var chalk = require('chalk');
+var config = require('../config');
+var merge = require('webpack-merge');
+var rm = require('rimraf');
+var shell = require('shelljs');
+var path = require('path');
+var webpack = require('webpack');
 var webpackConfig = merge(buildWebpackConfig, {
     plugins: [
         new webpack.ProgressPlugin()
@@ -26,6 +27,16 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
                 chunks: false,
                 chunkModules: false
             }) + '\n\n');
+        var assetsPath = path.join(__dirname, '../../../../../../statics/assets/content/administration');
+
+        console.log(chalk.cyan('  Moving files to path ' + assetsPath + '\n'));
+
+        shell.rm('-rf', assetsPath);
+        shell.mkdir('-p', assetsPath);
+        shell.config.silent = true;
+        shell.cp('-R', path.join(__dirname, '../dist/assets/content/administration/css'), assetsPath);
+        shell.cp('-R', path.join(__dirname, '../dist/assets/content/administration/js'), assetsPath);
+        shell.config.silent = false;
 
         console.log(chalk.cyan('  Build complete.\n'));
     });
--- resources/mixes/administration/build/watch.js
+++ resources/mixes/administration/build/watch.js
@@ -2,13 +2,14 @@ require('./check-versions')();
 
 process.env.NODE_ENV = 'production';
 
-var rm = require('rimraf');
-var path = require('path');
-var chalk = require('chalk');
-var merge = require('webpack-merge');
-var webpack = require('webpack');
-var config = require('../config');
 var buildWebpackConfig = require('./webpack.prod.conf');
+var chalk = require('chalk');
+var config = require('../config');
+var merge = require('webpack-merge');
+var path = require('path');
+var rm = require('rimraf');
+var shell = require('shelljs');
+var webpack = require('webpack');
 var webpackConfig = merge(buildWebpackConfig, {
     plugins: [
         new webpack.ProgressPlugin()
@@ -28,6 +29,16 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
                 chunks: false,
                 chunkModules: false
             }) + '\n');
+        var assetsPath = path.join(__dirname, '../../../../../../statics/assets/content/administration');
+
+        console.log(chalk.cyan('  Moving files to path ' + assetsPath + '\n'));
+
+        shell.rm('-rf', assetsPath);
+        shell.mkdir('-p', assetsPath);
+        shell.config.silent = true;
+        shell.cp('-R', path.join(__dirname, '../dist/assets/content/administration/css'), assetsPath);
+        shell.cp('-R', path.join(__dirname, '../dist/assets/content/administration/js'), assetsPath);
+        shell.config.silent = false;
 
         console.log(chalk.cyan(`  Build completed at ${(new Date()).toLocaleString()}.`));
         console.log(chalk.cyan('  Watching ...\n'));
