faef67d28f84d705992d1d003493b5ce90a0a4b0
twilroad <269044570@qq.com>
2017-11-07 18:33:25 +0800

    add definition of type for page

---
+++ src/GrqphQL/Types/PageType.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        TwilRoad <heshudong@ibenchu.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime      2017-11-07 18:32
+ */
+namespace Notadd\Content\GrqphQL\Types;
+
+use Notadd\Foundation\GraphQL\Abstracts\Type as AbstractType;
+
+/**
+ * Class PageType.
+ */
+class PageType extends AbstractType
+{
+    /**
+     * @return array
+     */
+    public function fields()
+    {
+        return [];
+    }
+
+    /**
+     * @return string
+     */
+    public function name()
+    {
+        // TODO: Implement name() method.
+    }
+}
